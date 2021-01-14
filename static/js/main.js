4257df39a7a8f4808c835d61ab994eec0c3cf9b3
YEONGEOL <dusrjf14@naver.com>
2021-01-14 15:39:00 +0900

    Revert "before "overwrite""
    This reverts commit eadee9ec8d99298ce1970e81e057b35b13319991.

---
+++ static/js/main.js
@@ -0,0 +1,26 @@
+var counter = 1;
+setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter > 4){
+        counter = 1;
+    }
+}, 5000);
+
+const searchBtn = document.querySelector(".search_btn")
+const cancelBtn = document.querySelector(".cancel_btn")
+const searchBox = document.querySelector(".search_box")
+const searchInput = document.querySelector("input")
+
+searchBtn.onclick = () =>{
+    searchBox.classList.add("active");
+    searchInput.classList.add("active");
+    searchBtn.classList.add("active");
+    cancelBtn.classList.add("active");
+}
+cancelBtn.onclick = () =>{
+    searchBox.classList.remove("active");
+    searchInput.classList.remove("active");
+    searchBtn.classList.remove("active");
+    cancelBtn.classList.remove("active");
+}
